15984049265d61ac8f7a40371ac40951bde80a96
waranya-sun <waranya.sun@gmail.com>
2021-10-12 17:56:28 +1100

    add basename

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Navbar from "./Navbar";
-import Home from "./Home.js";
-import Products from "./Products.js"
-import ProductDetails from "./ProductDetails";
-import Cart from "./Cart.js";
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home.js';
+import Products from './Products.js';
+import ProductDetails from './ProductDetails';
+import Cart from './Cart.js';
 
 function App() {
-  localStorage.setItem("cart", []);
+  localStorage.setItem('cart', []);
   const [cart, setCart] = useState(function () {
     let savedCart = [];
     try {
-      savedCart = JSON.parse(localStorage.getItem("cart")) || [];
+      savedCart = JSON.parse(localStorage.getItem('cart')) || [];
     } catch (error) {
       savedCart = [];
     }
@@ -19,47 +19,55 @@ function App() {
   });
 
   useEffect(() => {
-    localStorage.setItem("cart", JSON.stringify(cart));
+    localStorage.setItem('cart', JSON.stringify(cart));
   }, [cart]);
 
-  function handleProductAdd(newProduct){
+  function handleProductAdd(newProduct) {
     // check if item exists
-    const existingProduct = cart.find( product => product.id === newProduct.id);
-    if(existingProduct){
-      const updatedCart = cart.map( product => {
-        if(product.id === newProduct.id){
+    const existingProduct = cart.find(product => product.id === newProduct.id);
+    if (existingProduct) {
+      const updatedCart = cart.map(product => {
+        if (product.id === newProduct.id) {
           return {
             ...product,
-            quantity : product.quantity + 1
+            quantity: product.quantity + 1,
           };
         }
         return product;
       });
       setCart(updatedCart);
-    }else{
-    // product is a new one to the cart 
-    setCart([...cart, {...newProduct, quantity : 1}]);
+    } else {
+      // product is a new one to the cart
+      setCart([...cart, { ...newProduct, quantity: 1 }]);
     }
   }
 
-  function handleProductDelete(id){
-    const updatedCart = cart.filter( product => product.id !== id)
+  function handleProductDelete(id) {
+    const updatedCart = cart.filter(product => product.id !== id);
     setCart(updatedCart);
   }
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Navbar cart={cart} />
-      <div className="container">
+      <div className='container'>
         <Switch>
-          <Route exact path="/"><Home /></Route>
-          <Route exact path="/products"><Products
-            cart={cart}
-            onProductAdd={handleProductAdd}
-            onProductDelete={handleProductDelete}
-          /></Route>
-          <Route path="/products/:id"><ProductDetails onProductAdd={handleProductAdd} /></Route>
-          <Route exact path="/cart"><Cart cart={cart} /></Route>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/products'>
+            <Products
+              cart={cart}
+              onProductAdd={handleProductAdd}
+              onProductDelete={handleProductDelete}
+            />
+          </Route>
+          <Route path='/products/:id'>
+            <ProductDetails onProductAdd={handleProductAdd} />
+          </Route>
+          <Route exact path='/cart'>
+            <Cart cart={cart} />
+          </Route>
         </Switch>
       </div>
     </BrowserRouter>
